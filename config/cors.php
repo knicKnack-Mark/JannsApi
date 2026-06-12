b028e8b6ad13ae24e6e89dfd11e70093507a5454
knicKnack-Mark <markneilbertiz21@gmail.com>
2026-06-12 23:26:53 +0800

    Add CORS support for localhost and update routes for migration execution

--- config/cors.php
+++ config/cors.php
@@ -22,6 +22,7 @@ return [
         'allowed_origins' => [
             'http://localhost:3000',
             'http://127.0.0.1:3000',
+            'https://janns-staffs.vercel.app/'
         ],
 
         'allowed_origins_patterns' => [],
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/run-migrations-now', function () {
+    Artisan::call('migrate', ['--force' => true]);
+
+    return nl2br(Artisan::output());
+});
 
 Route::get('/', function () {
     return view('welcome');
